e650342392b158740c199ae44fe00460028693d8
Blake Swadling <blake@thecodemonkey.com.au>
2025-06-09 22:23:06 +1000

    clean up old crud

--- samba/runme.sh
+++
@@ -1,7 +0,0 @@
-docker run -d --restart unless-stopped \
-    -e SAMBA_DC_REALM='corp.example.net' \
-    -e SAMBA_DC_DOMAIN='EXAMPLE' \
-    -e SAMBA_DC_ADMIN_PASSWD='P@ssw0rd!' \
-    -e SAMBA_DC_DNS_BACKEND='SAMBA_INTERNAL' \
-    -v ${PWD}/data:/samba \
-     'samba-dc:latest'
--- traefik/redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stack rm $1
-sleep 5
-docker stack deploy -c docker-compose.yml $1
-docker service logs -f ${1}_traefik
-
