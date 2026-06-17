786949cf114ee85e9ceb0e96774440c036207ca4
thecodemonkeyau <blake@thecodemonkey.com.au>
2026-06-17 16:54:59 +1000

    run as unpriviledged user, check if swarm is ready

--- lock-n-load.sh
+++ lock-n-load.sh
@@ -1,13 +1,20 @@
 # if you have ipv6 isntalled you need to specify the ipv4 interface apparently
-docker swarm init --advertise-addr 192.168.68.222
+docker_status=$(docker info --format '{{.Swarm.LocalNodeState}}')
+ip_addr=$(ip -4 addr show wlan0 | grep -oP 'inet \K[\d.]+')
 
+if [[ "error" == "${docker_status}" ]]; then
+	echo "docker error"
+	exit 1
+fi
+
+# if we arent in a swarm, init one
+if [[ "inactive" == "${docker_status}" ]]; then
+	docker swarm init --advertise-addr ${ip_addr}
+fi
+
+# mk the config dir and lock it down to an unprivileged user
 sudo mkdir -p /docker/portainer
+sudo chown -R 1000:1000 /docker/portainer
 
-# FIXME: bad
-sudo chmod 777 /docker/portainer
-
-# lock this down
-# sudo chown docker-user /docker/portainer
-# sudo cmod 755 /docker/portainer
-
+# deploy the portainer stack
 docker stack deploy -c portainer-agent-stack.yml portainer
